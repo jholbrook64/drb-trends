97304ccacf37f4965fff9d28ef6806a348d28394
Jack Holbrook <jholbrook@usgs.gov>
2021-06-29 16:43:23 -0700

    first commit

--- _targets.R
+++ _targets.R
@@ -4,8 +4,10 @@ library(targets)
 source('src/site_selection_code.R')
 source('src/map_code.R')
 
+#install.packages("mapview")
+
 # Set target-specific options such as packages.
-tar_option_set(packages = c('sf', 'tidyverse'))
+tar_option_set(packages = c('sf', 'tidyverse', 'mapview'))
 
 # End this file with a list of target objects.
 list(
@@ -17,5 +19,17 @@ list(
   # example of how you could select some sites and map them using
   # all river segments from our modeling framework
   tar_target(site_map_png,
-             map_sites(annual_summary, 'in/network.rds'), format = 'file')
+             map_sites(annual_summary, 'in/network.rds'), format = 'file'),
+  
+  
+  # jack created:
+  
+  tar_target(annual_table,
+            meta_data(annual_summary), format = 'file'),
+  
+  tar_target(no_unique,
+             write_up(annual_summary)),
+  
+  tar_target(site_map_interactive,
+             map_tiles(annual_summary, 'in/network.rds'), format = 'file')
 )
--- src/site_selection_code.R
+++ src/site_selection_code.R
@@ -21,7 +21,47 @@ evaluate_annual <- function(in_file) {
     # this will produce repeated values per seg_id_nat
     mutate(n_all_time = sum(n_per_year)) %>% ungroup()
   
+  # returns dataframe
   return(annual)
+}
+
+meta_data <- function(annual) # this will be the function that does an eda of the data here:
+{
+  minimum_sampled_days <-  121 # four months
+  table <- annual %>% group_by(seg_id_nat) 
+  table <- subset(table, table[, 3] > minimum_sampled_days)
   
+  #table <- subset(table, table[, 1] == if_nan(table$seg_id_nat))
+  # %>% annual[annual[, 3(annual)]>= minimum_sampled_days] 
   
+  table <- table %>% group_by(seg_id_nat) %>% summarise(
+    n_per_year = mean(n_per_year),
+    n_all_time = mean(n_all_time)
+  )
+  
+  # we will begin an operation to remove years with low sampling
+  #table <- table %>% summarise()
+  
+  # for (year in vector) {
+  #   
+  # }
+  
+  readr::write_csv(table, 'out/groupedTable.csv')
+  return('out/groupedTable.csv')
+}
+
+if_nan <- function(x)
+{
+  is.nan(x)
+}
+
+write_up <- function(annual)
+{
+  # get number of unique segment id's.
+  no_unique <- sapply(annual, function(x) length(unique(x))) # the n_all_time will be 
+  print(no_unique)  # will only print once, for reference, below is the return:
+  
+  #seg_id_nat year n_per_year n_all_time 
+  #332         85        343        245
+  return(no_unique)
 }
