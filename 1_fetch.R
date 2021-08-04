61f43fb68ef06650493b943f1e9d39d5f0be1266
Jack Holbrook <jholbrook@usgs.gov>
2021-08-04 15:24:14 -0700

    comit new changes    #

--- 1_fetch.R
+++ 1_fetch.R
@@ -4,7 +4,7 @@ tar_option_set(packages = c('sf', 'tidyverse', 'mapview'))
 
 p1_targets_list <- list(
   tar_target(annual_summary, 
-             evaluate_annual('in/obs_temp_drb.rds')),
+               evaluate_annual('in/obs_temp_drb.rds')),
   
   tar_target(monthly_summary,
              evaluate_monthly('in/obs_temp_drb.rds')), #, format = 'file'
@@ -14,4 +14,6 @@ p1_targets_list <- list(
   
   tar_target(add_days,                            # take return of the previous target
              add_days_in_month(clean_Monthly)) #, format = 'file'
-)
+
+  # add_days goes straight to the function group_time, which return target time_grouped
+  )
