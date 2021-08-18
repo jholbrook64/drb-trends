4cd8d47f7986b16b814c88224cf4e4ef99ea2ae4
Jack Holbrook <jholbrook@usgs.gov>
2021-08-18 12:10:24 -0700

    commit with partially working map funciton. contain 1 rbind() 	error

--- 2_map.R
+++ 2_map.R
@@ -18,13 +18,19 @@ tar_option_set(packages = c('tidyverse', 'sf'))
 map_targets_list <- list(
   
   tar_target(boxplots_year, 
-             boxplot_func(year_data, 1), format = 'file'),
+             boxplot_func(Annual_regression, 1), format = 'file'),
   
   tar_target(boxplots_month, 
-             boxplot_func(month_data, 2), format = 'file'),
+             boxplot_func(meanofmean_regression, 2), format = 'file'),
   
+  tar_target(hist_year, 
+             hist_func(Annual_regression), format = 'file'),
+  
+ # 8-17-2021 the reason why this group by is needed is because 
+ # patterns can only branch over explicitly declared targets in the pipeline
+ # otherwise, the attribute to branch over is hidden from targets
   tar_group_by(months,
-               meanofmean_regression, Month),
+                meanofmean_regression, Month),
 
   # this is to create 12 plots that show the difference in the slope, represented as "year" for the regressions over each branch. 
   tar_target(site_map_pngs,
--- 2_map/src/data_map.R
+++ 2_map/src/data_map.R
@@ -23,22 +23,33 @@ map_sites <- function(data_for_trend_analysis_month, in_network)
   month_list <- c("January", "February", "March", "April", "May", "June", "July", "August",
                      "September", "October", "November", "December")
   monthname <- month_list[month]
+  
+  browser()
+  
   title <- "DRB wide warming trend for the month of "
   background <- st_as_sf(maps::map("state", plot = FALSE, fill = TRUE))
-  
+  DRB_states <- c("new jersey", "pennsylvania", "delaware", "maryland",  "new york")
+  background <- background[background$ID=="new jersey" | background$ID=="pennsylvania" | background$ID=="delaware" | background$ID=="maryland" | background$ID=="new york",]  # this should turn background data into a subset
+  do.call(rbind.data.frame, background)
+  # aoi <- st_bbox(c(xmin = min(network[2]$vertices$geometry[[1]]), xmax = max(network[2]$vertices$geometry[[1]]),
+  #                  ymax = max(network[2]$vertices$geometry[[2]]), ymin = min(network[2]$vertices$geometry[[2]])), 
+  #                crs = st_crs(4326))
   p <- ggplot(net_d) +
-    geom_sf(color = 'gray') +
+    
     # filter to sites with a n_all_time value, which indicates at least some data
    
      # color by how much data a segment has
-    geom_sf(data = filter(net_d, !is.na(`Warming Trend degC Year`)), aes(color =`Warming Trend degC Year`)) +
+    
     geom_sf(data = background, fill="white") +
+    geom_sf(color = 'grey') +
+    geom_sf(data = filter(net_d, !is.na(`Warming Trend degC Year`)), aes(color =`Warming Trend degC Year`)) +
     # geom_polygon(data = background, aes(x=long, y=lat, group=group),
     #             color="black", fill="gray") +
-    # coord_sf(xlim = c(-77, -0), ylim = c(0, 43), expand = FALSE) +
+    # coord_sf(xlim = c(min(in_network[2]$vertices$geometry[[1]]), max(in_network[2]$vertices$geometry[[1]])),
+    #          ylim = c(max(in_network[2]$vertices$geometry[[2]]), min(in_network[2]$vertices$geometry[[2]])), 
+    #          expand = FALSE) +
     scale_color_viridis_c(direction = -1, option = 'plasma', end = 1) +
     theme_bw() +
-    #st_bbox() +
     ggtitle(paste(title, monthname, sep = ""))+ 
     xlab(expression(paste(Longitude^o,~'N'))) +
     ylab(expression(paste(Latitude^o,~'W')))
@@ -53,24 +64,36 @@ boxplot_func <- function(regression_data, type)
 {
   if (type == 2) 
   {
-    regression_data$month <- as.factor(regression_data$month)
-    boxp <- ggplot(regression_data, aes(x = month, y = month_mean))+
-      geom_boxplot()+
-      ggtitle("Distribution of Stream Segment Means for Each Month")+ 
+    regression_data$Month <- as.factor(regression_data$Month)
+    #regression_data$Slope <- as.factor(regression_data$Slope)
+    # group_colors <- ifelse(levels(regression_data$Slope)>0.0, rgb(0.1,0.1,0.7,0.5),
+    #                 ifelse(levels(regression_data$Slope)<0.0, rgb(0.8,0.1,0.3,0.6),
+    #                             "grey90"  ))
+    month_list <- c("January", "February", "March", "April", "May", "June", "July", "August",
+                    "September", "October", "November", "December")
+    regression_data$Month <- month_list[regression_data$Month]
+    boxp <- ggplot(regression_data, aes(x = Month, y = Slope))+
+      #geom_boxplot(fill = as.numeric(range(cut(regression_data$Month, breaks = -6:6, label = FALSE)))) + 
+      geom_boxplot(fill = "blue") +
+      # aes(fill = after_scale()),size =1)+
+      # theme(legend.position="none") +
+      theme(axis.text.x = element_text(angle = 90)) +
+      scale_fill_distiller(palette = "RdYlGn", guide = "none") + 
+      ggtitle("Distribution of Stream Segment Trends for Each Month")+ 
       xlab("Individual Segments") +
-      ylab("site mean temperatures") +
-      theme(axis.text.x = element_text(angle = 90))
+      ylab("site Trends") 
+      
     
-    this_filename <-  file.path('2_map', 'out', 'boxplots_monthly.png')
+    this_filename <- file.path('2_map', 'out', 'boxplots_monthly.png')
     ggsave(filename = this_filename, boxp, height = 7, width = 5)
     return(this_filename)
   }
   else if (type == 1)
   {
     regression_data$seg_id_nat <- as.factor(regression_data$seg_id_nat)
-    boxp <- ggplot(regression_data, aes(x = seg_id_nat, y = annual_mean))+
+    boxp <- ggplot(regression_data, aes(x = seg_id_nat, y = Slope))+
       geom_boxplot()+
-      ggtitle("Distribution of Annual Means for Each Segment")+ 
+      ggtitle("Distribution of Stream Segment Trends for Each Segment")+ 
       xlab("Individual Segments") +
       ylab("Yearly mean temperatures") +
       theme(axis.text.x = element_text(angle = 90))
@@ -79,4 +102,20 @@ boxplot_func <- function(regression_data, type)
     ggsave(filename = this_filename, boxp, height = 7, width = 5)
     return(this_filename)
   }
+}
+
+hist_func <- function(regression_data)
+{
+  # takes annual data since the boxplots no longer works for annual data:
+  regression_data$seg_id_nat <- as.factor(regression_data$seg_id_nat)
+  boxp <- ggplot(regression_data, aes(x = Slope))+  #x = seg_id_nat, y = Slope
+    geom_histogram()+
+    ggtitle("Distribution of Stream Segment Trends for Each Segment")+ 
+    xlab("Individual Segments") +
+    ylab("Yearly mean temperatures") +
+    theme(axis.text.x = element_text(angle = 90))
+  
+  this_filename <-  file.path('2_map', 'out', 'boxplots_annual.png')
+  ggsave(filename = this_filename, boxp, height = 7, width = 5)
+  return(this_filename)
 }
