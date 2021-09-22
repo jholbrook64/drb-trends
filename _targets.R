db127f672aa6f5183896229615c350c425f98320
limnoliver <soliver@usgs.gov>
2021-09-22 11:00:53 -0500

    cleanup of new targets, commenting out annual for now. Simplified to a single list.

--- _targets.R
+++ _targets.R
@@ -6,5 +6,5 @@ source("1_fetch.R")
 source("2_map.R")
 source("3_summarize.R")
 
-c(fetch_targest_list, summarize_targets_list, combine_targets_list, map_targets_list, meta_summaries)
-#end  
+c(fetch_targest_list, map_targets_list, meta_summaries)
+#end
